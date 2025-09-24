f0c690bd92a75c295ffbee18bee8f1c97a93ca2d
massalfonso <massimo.alfonso13@gmail.com>
2025-09-24 11:54:46 -0700

    V25

--- response.php
+++ response.php
@@ -5,19 +5,20 @@
 </head>
 <body>
     <?php
+    /*
+    if (isset($_POST['models']) && isset($_POST['year'])) 
+    {
+        echo "<p>Your E46's Year: " . htmlspecialchars($_POST['year']) . "</p>";
+        echo "<p>Your E46 Model: " . htmlspecialchars($_POST['models']) . "</p>";
+    }
+    */
     if (isset($_POST['year'])) 
     {
         echo "<p>Your E46's Year: " . htmlspecialchars($_POST['year']) . "</p>";
     }
     
-    else if (isset($_POST['models'])) 
+    if -(isset($_POST['models'])) 
     {
-        echo "<p>Your E46 Model: " . htmlspecialchars($_POST['models']) . "</p>";
-    }
-
-    else if (isset($_POST['models']) && isset($_POST['year'])) 
-    {
-        echo "<p>Your E46's Year: " . htmlspecialchars($_POST['year']) . "</p>";
         echo "<p>Your E46 Model: " . htmlspecialchars($_POST['models']) . "</p>";
     }
     ?>
