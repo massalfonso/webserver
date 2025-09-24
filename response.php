3e0c72037a6e84f274e47dadf8952a2de98d61ab
massalfonso <massimo.alfonso13@gmail.com>
2025-09-24 10:09:27 -0700

    V16

---
+++ response.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        GET: <?php var_dump($_GET); ?>
+    </p>
+    
+    <p>
+        POST: <?php var_dump($_POST); ?>
+    </p>
+</body>
+</html>
