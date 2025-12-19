55c39b486aedde147abaf5ffc20b3ba3a76b26bf
massalfonso <massimo.alfonso13@gmail.com>
2025-12-19 11:39:52 -0500

    test

--- search.php
+++ search.php
@@ -5,6 +5,6 @@
 
         IP Address: <?php echo $_SERVER["REMOTE_ADDR"]; ?><br>
 
-        <?php header("Location: https://acme.co"); ?><br>
+        <?php header("Location: https://google.com"); ?><br>
     </body>
 </html>
