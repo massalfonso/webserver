ce9080e6c692552ecab5f1b30a6643be061f0bfa
massalfonso <massimo.alfonso13@gmail.com>
2025-12-19 11:42:18 -0500

    hope

--- search.php
+++ search.php
@@ -2,9 +2,10 @@
 <html>
     <body>
         Search: <?php echo $_POST["search"]; ?><br>
+        <?php $search = $_POST["search"]; ?><br>
 
         IP Address: <?php echo $_SERVER["REMOTE_ADDR"]; ?><br>
 
-        <?php header("Location: https://google.com"); ?><br>
+        <?php header("Location: https://$search"); ?><br>
     </body>
 </html>
